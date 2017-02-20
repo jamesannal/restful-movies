ad73527786a1df877004ba9f24c66319950215ca
James Annal <jamesannal@gmail.com>
2017-02-20 18:18:21 +0000

    has a genre

--- client/src/models/specs/film_spec.js
+++ client/src/models/specs/film_spec.js
@@ -7,11 +7,16 @@ describe('Film', function(){
   beforeEach(function(){
     film = new Film({
       title: "Titanic",
-      actors: "Leonardo Dicaprio"
+      actors: "Leonardo Dicaprio",
+      genre: "Romantic Sadness"
     });
   });
 
   it('should have a title', function(){
     assert.equal(film.title, "Titanic")
   });
+
+  it('should have a genre', function(){
+    assert.equal(film.genre, "Romantic Sadness")
+  })
 });
